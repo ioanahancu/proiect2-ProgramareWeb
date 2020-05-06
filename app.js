793a1019ed7575d618b19c0502a79a5833b60d8e
Ioana Hancu <anda-ioana.hancu@student.tuiasi.ro>
2020-05-06 14:35:23 +0300

    Rezolvare laborator 11 task 1 & 2

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
+var cookieParser=require('cookie-parser');
+var session=require('express-session');
+
 
 const fs = require('fs');
 
@@ -18,11 +21,25 @@ app.use(express.static('public'))
 app.use(bodyParser.json());
 // utilizarea unui algoritm de deep parsing care suportă obiecte în obiecte
 app.use(bodyParser.urlencoded({ extended: true }));
+// utilizare cookies
+app.use(cookieParser());
+// utilizare sesiuni
+app.use(session({
+	secret:'secret',
+	resave:false,
+	saveUninitialized:false,
+	cookie:{
+		maxAge:10000
+	}
+}));
 
-// la accesarea din browser adresei http://localhost:6789/ se va returna textul 'Hello World'
+// la accesarea din browser adresei http://localhost:6789/ se va returna view-ul index.ejs
 // proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => {
+	console.log(req.cookies);
+	res.render('index', { prajituri: req.cookies});
+});
 
 // la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
 app.get('/chestionar', (req, res) => {
@@ -52,5 +69,29 @@ app.post('/rezultat-chestionar', (req, res) => {
 	res.render('rezultat-chestionar', {rCorecte: rCorecte});
   })
 });
+
+app.get('/autentificare', (req,res) =>{
+	 res.render('autentificare', { prajituri: req.cookies });
+	 
+});
+
+app.post('/verificare-autentificare', (req, res) => {
+		// console.log(req.body);
+		var user = req.body['user'];
+		var passw = req.body['password'];
+		if( user == "Ioana" && passw == "buburuza"){
+			res.cookie('utlizator', user);		
+			res.redirect('/');			
+		}
+		else{
+			res.cookie('mesajEroare', 'autentificareInvalida');
+			res.redirect('/autentificare');
+		}		
+});
+
+// app.use(function(req, res, next) {
+// 	res.locals.user = req.session.user;
+// 	next();
+//   });
 
 app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:`));
