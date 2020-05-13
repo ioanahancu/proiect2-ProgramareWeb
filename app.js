6447c87dec58b36a96641a37e59d2ed767bf22e8
Ioana Hancu <anda-ioana.hancu@student.tuiasi.ro>
2020-05-13 15:51:47 +0300

    Rezolvare laborator 12

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ var cookieParser=require('cookie-parser');
 var session=require('express-session');
 
 
+var mysql = require('mysql');
+var con = mysql.createConnection({
+	host:"localhost",
+	user:"ioana",
+	password:"parola"
+	
+});
+
 const fs = require('fs');
 
 const app = express();
@@ -29,7 +37,7 @@ app.use(session({
 	resave:false,
 	saveUninitialized:false,
 	cookie:{
-		maxAge:10000
+		maxAge:100000
 	}
 }));
 
@@ -37,14 +45,39 @@ app.use(session({
 // proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
 app.get('/', (req, res) => {
-	// console.log(req.cookies);
-	if(req.session && req.session.username){
-		var utilizator = req.session.username;
-		res.render('index', { utilizator: utilizator});
+	var lista_produse=[];
+	if(con.state == 'disconnected')
+	{
+		con.connect(function(err){
+			if(err) throw err;
+			console.log("Connected");
+		});
 	}
-	else{
-		res.render('index', { utilizator: null});
-	}	
+	con.query("USE cumparaturi;");
+	con.query("SELECT * FROM produse", function(err, result){
+		if(err) throw err;
+		// console.log(result);
+		for(let i=0; i < result.length; i++)
+		{
+			lista_produse.push(result[i].id);
+			lista_produse.push(result[i].nume_produs);
+
+		}
+		// console.log(lista_produse);
+		if(req.session && req.session.username){
+			if(!req.session.hasOwnProperty('idProduse'))
+			{
+				req.session.idProduse = [];
+
+			}
+			var utilizator = req.session.username;
+			res.render('index', { utilizator: utilizator, lista_produse: lista_produse});
+		}
+		else{
+			req.session.idProduse = [];
+			res.render('index', { utilizator: null, lista_produse: lista_produse});
+		}	
+	});
 });
 
 // la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
@@ -65,7 +98,6 @@ app.get('/chestionar', (req, res) => {
 });
 
 app.post('/rezultat-chestionar', (req, res) => {
-  //console.log(req.body);
   fs.readFile('intrebari.json', (err, data) => {
 	if(err) throw err;  
 	var data = JSON.parse(data);
@@ -119,8 +151,89 @@ app.post('/verificare-autentificare', (req, res) => {
 });
 
 app.get('/delogare', (req, res) => {
-	req.session.destroy;
+	req.session.username=null;
 	res.redirect('/');
+});
+
+// la accesarea http://localhost:6789/creare-bd, 
+//serverul se conectează la serverul de baze de date
+// și creează o bază de date cu numele cumparaturi, 
+//în care creează o tabelă produse, 
+//după care răspunde clientului cu un redirect spre resursa /.
+app.get('/creare-bd', (req, res) => {
+	if(con.state == 'disconnected')
+	{
+		con.connect(function(err){
+			if(err) throw err;
+			console.log("Connected");
+		});
+	}
+
+	con.query("CREATE DATABASE IF NOT EXISTS cumparaturi", function(err, result){
+		if(err) throw err;
+		console.log("Database created");
+	});
+	con.query("USE cumparaturi;");
+	var sql = "CREATE TABLE IF NOT EXISTS produse(id int NOT NULL AUTO_INCREMENT, nume_produs varchar(255) NOT NULL, PRIMARY KEY (id));";
+	con.query(sql, function(err, result){
+		if(err) throw err;
+		console.log("Table created");
+	});
+	res.redirect('/');	
+});
+	
+
+
+app.get('/inserare-bd', (req, res)=> {
+	if(con.state == 'disconnected')
+	{
+		con.connect(function(err){
+			if(err) throw err;
+			console.log("Connected");
+		});
+	}
+	
+	con.query("USE cumparaturi");
+	var sql = "INSERT INTO produse(nume_produs) VALUES ?";
+	var values = [
+		['IT, Stephen King'],
+		['Harry Potter, J.K. Rowling'],
+		['LOTR, J.R.R. Tolkien'],
+		['Invata JavaScript, PW']
+	];
+	con.query(sql, [values], function(err, result){
+		if(err) throw err;
+		console.log("Number of records inserted: " + result.affectedRows);
+	});
+	res.redirect('/');
+});
+
+app.post('/adaugare-cos', (req, res) => {
+	req.session.idProduse.push(req.body['id']);	
+	console.log("Produs adăugat în coș");
+	res.redirect('/');
+});
+
+app.get('/vizualizare-cos', (req, res) => {
+	con.query("USE cumparaturi");
+	var values = req.session.idProduse;
+	var produseAdaugate=[];
+	if(values.length != 0)
+	{
+		var sql="SELECT nume_produs FROM produse WHERE id in ( ? ) ";
+		con.query(sql, [values], function(err, result){
+			if(err) throw err;
+			for(let i=0; i<result.length; i++)
+			{
+				produseAdaugate.push(result[i].nume_produs);
+			}
+			res.render('vizualizare-cos', { produse : produseAdaugate});
+		});	
+	}
+	else{
+		res.render('vizualizare-cos', {produse: null})
+	}
+	
 });
 
 app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:`));
